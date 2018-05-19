7a3c6ef53f8fa18a1df799082bfbdb449db55dc6
Dariusz <dariusz.mendocha12@gmail.com>
2018-05-20 01:38:42 +0200

    Add loading animations

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import {Link, Switch, Route, Redirect} from "react-router-dom";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {Switch, Route, Redirect, withRouter} from "react-router-dom";
 import { hot } from 'react-hot-loader';
 
 import Block from "./block/block.component";
 import Search from "./search/search.component";
 import AccountSummary from "./account-summary/account-summary.component";
+import {SearchStatus} from "./search/search.actions";
+import {DATA_LOADING_STATUS} from "./data-loading-status";
 
 export class App extends React.Component {
     render() {
+        const shouldBeElevated = this.props.searchEngineStatus === SearchStatus.FOUND_SOMETHING ||
+            this.props.dataHaveBeenLoaded? "elevated": "";
         return (
-            <div className="inner-container">
+            <div className={`inner-container ${shouldBeElevated}`}>
                 <Search/>
                 <Switch>
                     <Route exact path="/" component={() => <div/>}/>
@@ -22,4 +28,19 @@ export class App extends React.Component {
     }
 }
 
-export default hot(module)(App);
+App.propTypes = {
+    searchEngineStatus: PropTypes.string.isRequired,
+};
+
+function mapStateToProps(state) {
+    const dataHaveBeenLoaded = state.blockSummary.statusOfDataLoading === DATA_LOADING_STATUS.DATA_HAVE_BEEN_LOADED ||
+        state.accountSummary.statusOfDataLoading === DATA_LOADING_STATUS.DATA_HAVE_BEEN_LOADED;
+    return {
+        searchEngineStatus: state.searchEngine.status,
+        dataHaveBeenLoaded: dataHaveBeenLoaded,
+    };
+}
+
+const connectedApp = withRouter(connect(mapStateToProps)(App));
+
+export default hot(module)(connectedApp);
--- src/block/block.component.js
+++ src/block/block.component.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import ReactList from 'react-list';
-import TimeAgo from 'react-timeago'
+import TimeAgo from 'react-timeago';
 import NumberFormat from 'react-number-format';
 
 import {loadBlock, loadTransactionsByBlock} from "./block.actions";
@@ -30,30 +30,32 @@ export class Block extends React.Component {
     }
 
     renderTransaction(index, key) {
+        const dataLoadedClassName = this.props.statusOfDataLoading === DATA_LOADING_STATUS.DATA_HAVE_BEEN_LOADED
+            ? "loaded":"";
         const amount = this.props.transactions[index].tokenName === TRON_TOKEN_NAME?
             this.props.transactions[index].amount/TRX_PRECISION: this.props.transactions[index].amount;
         return (
             <div key={key} className="result-block">
-                <div className="data-piece loading loaded">
+                <div className={`data-piece loading ${dataLoadedClassName}`}>
                     <span className="balance result-title">
                         <NumberFormat value={amount} thousandSeparator={" "} displayType={'text'}/>
                          &nbsp;{this.props.transactions[index].tokenName}
                     </span>
                 </div>
 
-                <div className="data-piece loading loaded">
+                <div className={`data-piece loading ${dataLoadedClassName}`}>
                     <a className="link result-attr-1"> {this.props.transactions[index].transferFromAddress}</a>
                     <span className="icon href"/>
                 </div>
 
 
-                <div className="data-piece loading loaded">
+                <div className={`data-piece loading ${dataLoadedClassName}`}>
                     <span className="icon flow to" id="icon-direction"/>
                     <a className="link result-attr-2"> {this.props.transactions[index].transferToAddress}</a>
                     <span className="icon href"/>
                 </div>
 
-                <div className="data-piece loading loaded">
+                <div className={`data-piece loading ${dataLoadedClassName}`}>
                     <a className="link result-attr-3 truncate" onClick={this.goToTransactionDetails}>
                         <span className="icon hashtag"/>
                         {this.props.transactions[index].hash}
@@ -61,23 +63,27 @@ export class Block extends React.Component {
                     <span className="icon href"/>
                 </div>
             </div>
-        )
+        );
     }
 
 
     render() {
+        const dataLoadedClassName = this.props.statusOfDataLoading === DATA_LOADING_STATUS.DATA_HAVE_BEEN_LOADED
+            ? "loaded":"";
         return (
             <div className="results visible">
                 <div className="main-block">
-                    <h3 className="data-piece loading loaded" id="h3-main">BLOCK HEIGHT</h3>
-                    <span className="data-piece loading loaded" id="search-result">{this.props.selectedBlock.number}</span>
-                    <a className="data-piece loading loaded balance" id="data-main">
+                    <h3 className={`data-piece loading ${dataLoadedClassName}`} id="h3-main">BLOCK HEIGHT</h3>
+                    <span className={`data-piece loading ${dataLoadedClassName}`} id="search-result">
+                        {this.props.selectedBlock.number}
+                    </span>
+                    <a className={`data-piece loading ${dataLoadedClassName} balance`} id="data-main">
                         <TimeAgo date={this.props.selectedBlock.timestamp}/>
                     </a>
-                    <span className="data-piece loading loaded result-block-hash">
+                    <span className={`data-piece loading ${dataLoadedClassName} result-block-hash`}>
                     # {this.props.selectedBlock.hash}
                     </span>
-                    <h3 className="data-piece loading loaded" id="h3-secondary">TRANSACTIONS</h3>
+                    <h3 className={`data-piece loading ${dataLoadedClassName}`} id="h3-secondary">TRANSACTIONS</h3>
                 </div>
                 <div style={{overflow: 'auto', maxHeight: 200}}>
                     <ReactList
@@ -86,7 +92,7 @@ export class Block extends React.Component {
                     />
                 </div>
             </div>
-        )
+        );
     }
 }
 
